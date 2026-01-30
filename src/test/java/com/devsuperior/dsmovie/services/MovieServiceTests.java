02839d576c9e9d673ff6d20db5bca5e4974ac205
Gabriel Garcia <gabrielproaj@gmail.com>
2026-01-30 18:36:31 -0300

    FindById retornando dado com id existente

--- src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
@@ -1,22 +1,55 @@
 package com.devsuperior.dsmovie.services;
 
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.MovieEntity;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.tests.MovieFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 public class MovieServiceTests {
-	
+
 	@InjectMocks
 	private MovieService service;
-	
+
+	@Mock
+	private MovieRepository repository;
+
+	private MovieEntity movie;
+	private MovieDTO movieDTO;
+
+	private Long existingId, nonExistingId;
+
+	@BeforeEach
+	public void setUp() throws Exception{
+		movie = MovieFactory.createMovieEntity();
+		movieDTO = new MovieDTO(movie);
+		existingId = 1L;
+		nonExistingId = 2L;
+
+		Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(movie));
+		Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+	}
+
 	@Test
 	public void findAllShouldReturnPagedMovieDTO() {
 	}
 	
 	@Test
 	public void findByIdShouldReturnMovieDTOWhenIdExists() {
+		MovieDTO result = service.findById(existingId);
+
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(existingId, result.getId());
 	}
 	
 	@Test
